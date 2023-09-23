f794585b18036787d2998cd90dae901d3e6653bb
vikesh <vikeshkr2ins@gmail.com>
2023-09-23 16:40:43 +0530

    Initial Commit

---
+++ psa_java/src/day4/B.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class B {
+	int x= 10;
+	public static void main(String[] args) {
+		
+		b1.test();// Error
+		
+		
+	}
+	public  void test() {
+		B b1 = new B();
+		System.out.println(b1.x);
+		
+	}
+
+}
---
+++ psa_java/src/day4/C.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class C {
+	int x= 10;
+	int y = 30;
+	public static void main(String[] args) {
+		C c1 = new C();
+		System.out.println(c1.x);
+		System.out.println(c1.y);
+	}
+
+}
