6ba09ba3022b9c95a466dc27ed8342ef2e37aa69
vikesh <vikeshkr2ins@gmail.com>
2023-09-24 01:18:38 +0530

    Initial Commit

---
+++ psa_java/src/day4/D.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class D {
+	int x = 10;
+	int y = 30;
+	public static void main(String[] args) {
+		D d1 = new D ();
+		D d2 = new D ();
+		
+		System.out.println(d1.x);
+		System.out.println(d2.y);
+		
+		
+	}
+
+}
