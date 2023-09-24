fb9cf69ac26e3542baf79d6c4318bb49e6b7be7e
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-24 19:56:01 +0530

    E.java

---
+++ psa_java/src/day7/E.java
@@ -0,0 +1,9 @@
+package day7;
+// without initializing we cannot use local variable.
+public class E {
+	public static void main(String[] args) {
+		E e1;
+		System.out.println(e1);// Error
+	}
+
+}
