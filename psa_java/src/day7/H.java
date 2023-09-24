55a4440da8951431b0416d727c91ef521fb062df
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-24 19:58:43 +0530

    local reference variable

---
+++ psa_java/src/day7/H.java
@@ -0,0 +1,12 @@
+package day7;
+//static and non-static variable names cannot be same.
+
+public class H {
+	static int x = 10;//Error
+	int x = 20;// Error
+	// Duplicate variable "X"
+	public static void main(String[] args) {
+		
+	}
+
+}
