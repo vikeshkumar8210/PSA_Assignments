d0609587ff5ca33d0d22882d5c2200d386ce60a4
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-24 19:57:02 +0530

    local reference variable

---
+++ psa_java/src/day7/F.java
@@ -0,0 +1,13 @@
+package day7;
+// Note:- Non-static & local variable names can be same.
+
+public class F {
+	int x =50;// non-static
+	public static void main(String[] args) {
+		int x = 10; // local
+		
+		
+		
+	}
+
+}
