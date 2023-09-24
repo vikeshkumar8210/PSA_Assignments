067ffff80653a67d34c7583e9d8e1d0f01819074
vikesh <vikeshkr2ins@gmail.com>
2023-09-24 19:09:28 +0530

    Reference variable

---
+++ psa_java/src/day7/A.java
@@ -0,0 +1,13 @@
+package day7;
+// These variable can store either null or object address only.
+// Data types of this variable will be class name.
+public class A {
+	public static void main(String[] args) {
+		A a1 = new A();
+		System.out.println(a1);
+		
+		A a2 = new A ();
+		System.out.println(a2);
+	}
+
+}
