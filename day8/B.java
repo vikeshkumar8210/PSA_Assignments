9be22b730dcdd8de8d0ea6f8cb0c4bfd3b8be26c
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-25 22:48:19 +0530

    Add files via upload

---
+++ day8/B.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class B {
+	public static void main(String[] args) {
+		B b1 = new B ();
+		int x = b1.test();
+		System.out.println(x);
+	}
+	public int test() {
+		return 100;
+		
+	}
+
+}
