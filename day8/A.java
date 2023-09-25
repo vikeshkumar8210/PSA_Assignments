2adb9c9f9bc1e4055117361740bb7f384cb912bf
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-25 23:24:51 +0530

    void method

---
+++ day8/A.java
@@ -0,0 +1,13 @@
+package day8;
+// Void method cannot return any value. Hence below program throw error.
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+	}
+	public void test() {
+		return 100;//error
+	}
+
+}
