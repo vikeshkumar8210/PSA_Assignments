c4913bc40aba06708b70958d2f9388f877acea07
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-28 00:39:41 +0530

    Method Argument

---
+++ day8/G.java
@@ -0,0 +1,13 @@
+package day8;
+
+public class G {
+	public static void main(String[] args) {
+		G g1 = new G();
+		g1.test(100, 10.3);
+	}
+	public void test(int x, double y) {
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
