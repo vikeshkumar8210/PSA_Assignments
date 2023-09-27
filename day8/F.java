0ba4a5b2d5f24a286a80540798ff08704d10077d
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-28 00:38:41 +0530

    Method Argument

---
+++ day8/F.java
@@ -0,0 +1,14 @@
+package day8;
+// Method argument example
+public class F {
+	public static void main(String[] args) {
+		F f1 = new F ();
+		f1.test(100);
+		
+	}
+	public void test(int x) {// local variable
+		System.out.println(x);// 100
+		
+	}
+
+}
