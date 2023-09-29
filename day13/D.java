1d29f771c2643b045c3030d78f4f3f92204cee98
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-29 16:50:59 +0530

    Inheritance

--- day13/D.java
+++ day13/D.java
@@ -1,6 +1,6 @@
 package day13;
 // Where a subclass extends another subclass, creating a chain or hierarchy of classes is called as multi-level inheritance.
-
+// Classes in java does not support multiple inheritance but interfaces in java support multiple inheritance.
 public class D extends E{
 	public static void main(String[] args) {
 		D d1 = new D ();
