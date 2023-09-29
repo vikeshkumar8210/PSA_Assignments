580b8a1b690fe5eb543dfca9cf1e2fd2bfc805e7
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-30 00:36:44 +0530

    Override Annotation

---
+++ day15/E.java
@@ -0,0 +1,9 @@
+package day15;
+// @Override Annotation provide additional information to the compiler to check weather overriding is happening or not.
+
+public class E {
+	public void test2() {
+		System.out.println(100);
+	}
+
+}
