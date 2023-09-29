2bf230e7b09e15a65912e09526ff01fa0fceaea2
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-30 00:39:34 +0530

    @Override Annotation

---
+++ day15/G.java
@@ -0,0 +1,8 @@
+package day15;
+// Annotation in java can add to your java code to provide additional information to the compiler, development tools and runtime environment.
+public class G {
+	public void test(int x) {
+		System.out.println(x);
+	}
+
+}
