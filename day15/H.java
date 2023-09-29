8a339bcc6c296a59db84d9de955b99db82dff35c
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-09-30 00:41:06 +0530

    @Override Annotation

---
+++ day15/H.java
@@ -0,0 +1,15 @@
+package day15;
+
+public class H extends G{
+	@Override
+	public void test2(int y) {
+		System.out.println(y);
+		
+	}
+	public static void main(String[] args) {
+		H h1 = new H();
+		h1.test(0);
+		
+	}
+
+}
