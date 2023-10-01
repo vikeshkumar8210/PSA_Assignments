8a33e94edb7d052e8c246ccadd7aab3b586611f2
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-01 15:33:28 +0530

    Abstraction

--- day17/Bank.java
+++ day17/Bank.java
@@ -2,6 +2,7 @@ package day17;
 // In java, an interface is a core concept that allows you to define a contract for classes to follow.
 // Static incomplete methods are not allowed inside interface.
 // Only abstract methods are allowed inside interfaces.
+// Interface supports multiple inheritance in java.
 public interface Bank {
 	public void TransferAmount();
 	public void Statements();
