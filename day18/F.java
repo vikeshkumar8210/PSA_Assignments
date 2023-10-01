6532d7e26dca4ded7d77817e7b69f5b73e647ce8
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-01 16:09:24 +0530

    Interfaces

---
+++ day18/F.java
@@ -0,0 +1,10 @@
+package day18;
+// Reference variable of an interface can be created but object of Interface cannot be created.
+
+public class F {
+	public static void main(String[] args) {
+		E e1 = new E();// error
+		E e1 = null;
+	}
+
+}
