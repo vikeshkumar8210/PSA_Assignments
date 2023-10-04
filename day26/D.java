baa9edb82f10cfc45b0e250c80a980f72d739746
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 01:11:32 +0530

    continue(condition)

---
+++ day26/D.java
@@ -0,0 +1,14 @@
+package day26;
+
+public class D {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			 if (i==2) {
+				continue ;
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
