e0a6f23f2ac7a87a226c4b9ba85e12d25cd2f6e1
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 01:08:55 +0530

    break(condition)

---
+++ day26/C.java
@@ -0,0 +1,14 @@
+package day26;
+
+public class C {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			 if (i==2) {
+				break ;
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
