5e955925cce957dec8915a42311348d7b5a0f752
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 01:15:01 +0530

    labeled break(condition)

---
+++ day26/E.java
@@ -0,0 +1,14 @@
+package day26;
+
+public class E {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			 x: if (i==2) {
+				break x ;
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
