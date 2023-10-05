16dca3739a5b5e0cd8c8cdc3766c674bbe7ce1df
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 23:04:26 +0530

    switch case (condition)

---
+++ day27/D.java
@@ -0,0 +1,25 @@
+package day27;
+
+public class D {
+	public static void main(String[] args) {
+		int key = 2;
+		switch (key) {
+		case 1:
+			System.out.println(1);
+			break ;
+			
+		case 2:
+			System.out.println(200);
+			break;
+			
+		case 3:
+			System.out.println(3);
+			break ;
+			
+		default:
+			System.out.println("No match is found");
+			break;
+		}
+	}
+// output :- 200
+}
