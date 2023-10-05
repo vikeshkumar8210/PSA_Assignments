a7c59db653bd43d42d6f9a101e34cbb5405eef1a
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 22:56:09 +0530

    For loop with Infinite-loop

---
+++ day27/A.java
@@ -0,0 +1,10 @@
+package day27;
+// for loop
+public class A {
+	public static void main(String[] args) {
+		for (int i=10; i>0; i--) {// 10,9,8,7,6,5,4,3,2,1
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ day27/B.java
@@ -0,0 +1,11 @@
+package day27;
+
+public class B {
+	public static void main(String[] args) {
+		// Infinite loop 
+		for(;;) {
+			System.out.println(100);
+		}
+	}
+
+}
