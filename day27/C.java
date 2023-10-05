2fbf28ac55883bee706abcb0f8d75dba988ad0fd
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 22:59:52 +0530

    Do while loop

---
+++ day27/C.java
@@ -0,0 +1,14 @@
+package day27;
+// Do-while loop
+public class C {
+	public static void main(String[] args) {
+		int x=0;
+		do {
+			System.out.println(x);
+			// 0,1,2
+			x++;
+		}
+		while (x < 3);
+	}
+
+}
