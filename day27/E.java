65f2ef5f07430b23efa88118d21065b2cef59004
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-05 23:34:12 +0530

    If-esle(condition)

---
+++ day27/E.java
@@ -0,0 +1,20 @@
+package day27;
+
+public class E {
+	public static void main(String[] args) {
+		int marks=90;
+		if(marks==50) {
+			System.out.println(500);
+		}
+		else if(marks==40) {
+			System.out.println(400);
+		}
+		else if(marks==90) {
+			System.out.println(900);
+		}
+		else {
+			System.out.println(4);
+		}
+	}
+
+}
