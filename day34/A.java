ef669a3b6611f42a64179b447cec7f7778909dab
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-23 22:37:20 +0530

    Serialization and De-Serialization

--- day34/A.java
+++ day34/A.java
@@ -1,5 +1,7 @@
 package day34;
 
+// Serialization and De-serialization are process in java that allow you to convert object into a byte stream (serialization) and then recreate objects from that byte stream(de-serialization).
+
 import java.io.Serializable;
 
 public class A implements Serializable{
