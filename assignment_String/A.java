ff4abde9de10d702849a9d12a93a71dc81e8e482
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-25 13:38:42 +0530

    Count the number of words in a given String

--- assignment_String/A.java
+++ assignment_String/A.java
@@ -2,10 +2,11 @@ package assignment_String;
 // Count the number of words in a given String
 public class A {
 	public static void main(String[] args) {
-		String str="Pankaj Sir Academy";
+		String str=" Pankaj Sir Academy ";
 		String[] s=str.split(" ");
 		System.out.println(s.length);
 	}
 
 }
-//Output:- 3
+//Output:- 5
+// It will also count all the spaces
