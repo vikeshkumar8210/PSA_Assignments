8b110d7c62e17dd6cd5293679aaaf22a5af296ae
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-25 13:31:54 +0530

    Reverse a String

---
+++ assignment_String/ReverseString.java
@@ -0,0 +1,12 @@
+package assignment_String;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String str ="Pankaj sir Acdamey";
+		for(int i=str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
+// output:- yemadcA ris jaknaP
