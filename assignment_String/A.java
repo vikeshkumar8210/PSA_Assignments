f7a0195711eeefaff8330072c27282fb21a5a78d
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-25 13:36:49 +0530

    Count the number of word in a given String

---
+++ assignment_String/A.java
@@ -0,0 +1,11 @@
+package assignment_String;
+// Count the number of words in a given String
+public class A {
+	public static void main(String[] args) {
+		String str="Pankaj Sir Academy";
+		String[] s=str.split(" ");
+		System.out.println(s.length);
+	}
+
+}
+//Output:- 3
