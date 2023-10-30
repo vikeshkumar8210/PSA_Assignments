b51f304c37a53bca7d27722a74e0380a402c1f60
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-30 22:16:30 +0530

    JDBC

--- day36/B.java
+++ day36/B.java
@@ -1,5 +1,24 @@
 package day36;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class B {
+	public static void main(String[] args) {
+		//Connect to database
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/psa","root","9572");
+			
+			//Perform SQL Operation
+			Statement stmnt = con.createStatement();
+			stmnt.executeUpdate("delete from registration where email='stallin@gmail.com'");
+			
+			//close
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
