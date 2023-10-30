6fb85955235381034e9103ad6905c983a1c83bc1
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-30 22:25:58 +0530

    JDBC (Sql Command)

---
+++ day36/C.java
@@ -0,0 +1,25 @@
+package day36;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class C {
+	public static void main(String[] args) {
+		//Connect to database
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/psa","root","password");
+			
+			//Perform SQL Operation
+			Statement stmnt = con.createStatement();
+			stmnt.executeUpdate("Update registration set mobile='9678452589' where email='stallin@gmail.com'");
+			
+			//close
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
