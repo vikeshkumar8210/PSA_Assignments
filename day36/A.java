354f597a1ecd59c25c7af873d47101c59b971ee5
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-30 22:05:41 +0530

    JDBC (sql command)

---
+++ day36/A.java
@@ -0,0 +1,26 @@
+package day36;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class A {
+	public static void main(String[] args) {
+		try {
+			// Connect to Database
+			Connection con = DriverManager.getConnection("jdbc:mysql:127.0.0.1:3306/psa","root","9572");
+			// Perform SQL Operations
+			Statement stmnt= con.createStatement();
+			stmnt.executeUpdate("insert into registration values('vikesh','jharkhand','vikesh@gmail.com'");
+			
+			//close
+			con.close();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ day36/B.java
@@ -0,0 +1,5 @@
+package day36;
+
+public class B {
+
+}
