2ac5d149c68cf3925c3ad24552bae3fc42a1327c
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-10-31 22:51:40 +0530

    Finally

---
+++ day37/B.java
@@ -0,0 +1,18 @@
+package day37;
+//Finally (It is a Extension of try and catch block)
+
+public class B {
+	public static void main(String[] args) {
+		try {
+			int x=10/0;	
+		}catch(Exception e) {
+		
+			System.out.println(e);
+		}finally {
+			System.out.println(1000);
+		}
+	}
+
+}
+
+//OutPut:-
