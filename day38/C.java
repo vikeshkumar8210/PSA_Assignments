d324f14544e4cc928bc98f32062dbf23e848c1f0
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-11-01 21:23:34 +0530

    multiple catch block

---
+++ day38/C.java
@@ -0,0 +1,18 @@
+package day38;
+
+public class C {
+	public static void main(String[] args) {
+		try {
+			Integer.parseInt("mikechup");
+		}
+		catch(ArithmeticException e) {
+			System.out.println(1);
+		}catch(NullPointerException e) {
+			System.out.println(2);
+		}catch(Exception e) {
+			System.out.println(3);
+		}
+	}
+
+}
+OutPut:-
