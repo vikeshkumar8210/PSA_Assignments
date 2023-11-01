9c2793968a7b6ebf77c44f216bd06b6582cc91ab
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-11-01 21:21:09 +0530

    Multiple catch block

---
+++ day38/B.java
@@ -0,0 +1,19 @@
+package day38;
+
+public class B {
+	int x=10;
+	public static void main(String[] args) {
+		try {
+			B b1=null;
+			System.out.println(b1.x);
+		}
+		catch(ArithmeticException e) {
+			System.out.println(1);
+		}catch(NullPointerException e) {
+			System.out.println(2);
+		}catch(Exception e) {
+			System.out.println(3);
+		}
+	}
+
+}
